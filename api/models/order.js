b461565c904a65008062dda5ab542afae5e69172
mustafaalpyanikoglu <79158705+mustafaalpyanikoglu@users.noreply.github.com>
2022-10-30 12:58:14 +0300

    feat: jwt added but user deletion bug

--- api/models/order.js
+++ api/models/order.js
@@ -4,7 +4,7 @@ const orderSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
     product: { type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true },
     quantity: { type: Number, default: 1 } //bana hiçbirşey iletmezse default olarak 1 sipariş alır
-})
+});
 
 
 module.exports = mongoose.model('Order', orderSchema);
--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const mongoose = require('mongoose');
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
+const userRoutes = require('./api/routes/user');
 
 mongoose.connect("mongodb+srv://mustafaalpyanikoglu:" + process.env.MONGO_ATLAS_PW + "@node-rest-shop.v3hrmf6.mongodb.net/?retryWrites=true&w=majority", );
 
@@ -35,6 +36,7 @@ app.use((req, res, next) => {
 // Routes which should handle requests
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
+app.use('/user', userRoutes);
 
 app.use((req, res, next) => {
     const error = new Error('Not found');
